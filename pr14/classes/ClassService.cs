9e5c7d8ac05bc9368c9d313fd3f3a1ad7c5b576a
elenabor2003@gmail.com <elenabor2003@gmail.com>
2022-12-13 14:59:16 +0300

    13.12.22

---
+++ pr14/classes/ClassService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr14.classes
+{
+    public partial class Service
+    {
+        public string Price
+        {
+            get
+            {
+                
+            }
+        }
+    }
+}
