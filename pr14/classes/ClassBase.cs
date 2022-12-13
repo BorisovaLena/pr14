eb9e052590037f9d96e9ff828eb9ab18495cf005
elenabor2003@gmail.com <elenabor2003@gmail.com>
2022-12-13 14:53:30 +0300

    Добавьте файлы проекта.

---
+++ pr14/classes/ClassBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr14.classes
+{
+    internal class ClassBase
+    {
+        public static Entities Base;
+    }
+}
