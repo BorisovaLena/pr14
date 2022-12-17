2b0a33fbb544e9fcf239f3df32cf6e0f61e282b5
Lena_Borisova <elenabor2003@gmail.com>
2022-12-17 20:22:09 +0300

    17/12/22

--- pr14/classes/ClassService.cs
+++ pr14/classes/ClassService.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
-namespace pr14.classes
+namespace pr14
 {
     public partial class Service
     {
@@ -12,7 +13,30 @@ namespace pr14.classes
         {
             get
             {
-                
+                if(Discount!=null)
+                {
+                    double f = (double)Cost - (double)Cost * (double)Discount;
+                    return string.Format("{0:C2} {1:C2} за {2} минут *скидка {3} %", Cost, f, DurationInSeconds / 60, Discount*100);
+                }
+                else
+                {
+                    return string.Format("{0:C2} за {1} минут", Cost, DurationInSeconds / 60);
+                }
+            }
+        }
+
+        public SolidColorBrush Color
+        {
+            get
+            {
+                if(Discount!=null)
+                {
+                    return Brushes.Green;
+                }
+                else
+                {
+                    return Brushes.White;
+                }
             }
         }
     }
--- pr14/pages/PageService.xaml.cs
+++ pr14/pages/PageService.xaml.cs
@@ -20,10 +20,60 @@ namespace pr14.pages
     /// </summary>
     public partial class PageService : Page
     {
+        List<Service> listFilter;
         public PageService()
         {
             InitializeComponent();
             listServices.ItemsSource = classes.ClassBase.Base.Service.ToList();
+            cmbSort.SelectedIndex = 0;
+        }
+
+        void Filter()
+        {
+            listFilter = classes.ClassBase.Base.Service.ToList();
+            if (!string.IsNullOrWhiteSpace(tbSearch.Text)) //поиск
+            {
+                listFilter = listFilter.Where(z => z.Title.ToLower().Contains(tbSearch.Text.ToLower())).ToList();
+            }
+            switch (cmbSort.SelectedIndex)
+            {
+                case 1:
+                    listFilter = listFilter.Where(z => z.Discount < 0.05).ToList();
+                    break;
+                case 2:
+                    listFilter = listFilter.Where(z => z.Discount > 0.05 && z.Discount<0.15).ToList();
+                    break;
+                case 3:
+                    listFilter = listFilter.Where(z => z.Discount > 0.15 && z.Discount < 0.3).ToList();
+                    break;
+                case 4:
+                    listFilter = listFilter.Where(z => z.Discount > 0.3 && z.Discount < 0.7).ToList();
+                    break;
+                case 5:
+                    listFilter = listFilter.Where(z => z.Discount > 0.7).ToList();
+                    break;
+            }
+            listServices.ItemsSource = listFilter;
+
+            int zap = listFilter.Count;
+            int vzap = classes.ClassBase.Base.Service.Count();
+
+            tbData.Text = zap + " из " + vzap;
+
+            if (listFilter.Count == 0)
+            {
+                MessageBox.Show("нет записей");
+            }
+        }
+
+        private void cmbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
         }
     }
 }
